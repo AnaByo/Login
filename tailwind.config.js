dac1c2e8e448a4f79c9eb3c3303174ad6278fcd9
AnaByo <114250758+AnaByo@users.noreply.github.com>
2023-08-07 10:10:56 -0300

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    colors: {
+      bluebaby: '#F0F5F9',
+      blueprincipal: '#05AFF2',
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
